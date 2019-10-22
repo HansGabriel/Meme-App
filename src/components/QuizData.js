4fadcb90ddac6b71362473cbfb535aa4e9c863f1
Hans Daduya <hansdaduya@yahoo.com>
2019-10-23 00:24:32 +0800

    second commit

---
+++ src/components/QuizData.js
@@ -0,0 +1,26 @@
+export const QuizData = [
+    {
+        id: 0,
+        question: 'What is the capital Of India?',
+        options: ['New Delhi', 'Mumbai', 'Kolkotta'],
+        answer: 'New Delhi'
+    },
+    {
+        id: 1,
+        question: 'Who is the owner of Tesla?',
+        options: ['Nicolo Tesla', 'Elon Musk', 'Marc Zuckerburg'],
+        answer: 'Elon Musk'
+    },
+    {
+        id: 2,
+        question: 'Who is the owner of Facebook?',
+        options: ['Mark Zuckerburg', 'Steve Jobs', 'Bill Gates'],
+        answer: 'Mark Zuckerburg'
+    },
+    {
+        id: 3,
+        question: 'What is the national food of the Philippines?',
+        options: ['Halo-halo', 'Lechon', 'Adobo'],
+        answer: 'Adobo'
+    },
+]
